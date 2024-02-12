22e55c3265c5260a17545387846cafde3816fea7
gowtham m <gowthameha1921@gmail.com>
2024-02-12 16:02:06 +0530

    order

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { PDFViewer } from '@react-pdf/renderer';
+
+import ExportPDFView from "./OrderAccecptance/Index"
+
+
+
+function App() {
+  return (
+    <div className='main1'>
+      <PDFViewer width='100%' height='100%'>
+        
+         <ExportPDFView />
+
+        
+
+      </PDFViewer>
+    </div>
+  );
+}
+
+export default App;
